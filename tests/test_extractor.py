cba4bbfed12e4203678c22d9697385fee7017ecf
Alois Klink <alois.klink@soton.ac.uk>
2019-10-21 16:16:39 +0100

    Improve tests

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -10,4 +10,24 @@ TEST_IMAGE_PATH = pathlib.Path(__file__).parent / TEST_IMAGE_RELATIVE_PATH
 
 def test_get_thermal():
     with FlirExtractor() as flir_extractor:
-        flir_extractor.get_thermal(TEST_IMAGE_PATH)
+        thermal_a = flir_extractor.get_thermal(TEST_IMAGE_PATH)
+        # should work when loading a str variable
+        thermal_b = flir_extractor.get_thermal(str(TEST_IMAGE_PATH))
+        # loading the same file twice should get the same result
+        assert (thermal_a == thermal_b).all()
+
+        with pytest.raises(TypeError):
+            # should raise typeError with incorrect type
+            flir_extractor.get_thermal(tuple())
+
+        with pytest.raises(FileNotFoundError):
+            flir_extractor.get_thermal("not existant file.jpg")
+
+        with pytest.raises(IsADirectoryError):
+            flir_extractor.get_thermal(pathlib.Path(__file__).parent)
+
+def test_get_thermal_batch():
+    with FlirExtractor() as flir_extractor:
+        flir_extractor.get_thermal_batch(
+            (TEST_IMAGE_PATH, str(TEST_IMAGE_PATH))
+        )
