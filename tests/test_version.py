8d28778627a190d27153902ffc756ccc9f3956de
Alois Klink <alois.klink@soton.ac.uk>
2019-11-01 18:42:39 +0000

    Test version based on pyproject.toml file

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,11 @@
+import tomlkit
+
 import flirextractor
 
 
 def test_version():
-    assert flirextractor.__version__ == "0.1.0"
+    with open("./pyproject.toml", "r") as tomlfile:
+        contents = tomlfile.read()
+        parsed_contents = tomlkit.parse(contents)
+    loaded_version = parsed_contents["tool"]["poetry"]["version"]
+    assert flirextractor.__version__ == loaded_version
